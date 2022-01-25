e060e2f0ec6c13bde30733826333758510a679b5
OB <nuor33333333@gmail.com>
2022-01-25 17:00:58 +0200

    1.5 half work done

---
+++ trapezoid.js
@@ -0,0 +1,5 @@
+a = prompt("Side A");
+b = prompt("Side B");
+h = prompt( "Height and Base Line" );
+S = 1/2(a+b)*h;
+alert(S);
---
+++ triangle.js
@@ -0,0 +1,5 @@
+a = prompt("Side A");
+b = prompt("Side B");
+corneraRad = prompt( "Corner a Rad" );
+S = 1/2*a*b*Math.sin(corneraRad);
+alert(S);
